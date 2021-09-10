07370c2fb6879ba31d99a65577ed546a9f3d2e94
Raymond <raymondtseng@pacificamerican.org>
2021-09-10 12:24:55 +0800

    j

--- Assignment 5/AgeMessage/AgeMessage.java
+++ Assignment 5/AgeMessage/AgeMessage.java
@@ -9,11 +9,14 @@ public class AgeMessage {
 
         if(age<16){
             System.out.print("You can't drive.");
-        }else if(age>=16 && age<18){
+        }
+        if(age>=16 && age<18){
             System.out.print("You can drive but not vote.");
-        }else if(age>=18 && age<25){
+        }
+        if(age>=18 && age<25){
             System.out.print("You can vote but not rent a car.");
-        }else if(age>=25){
+        }
+        if(age>=25){
             System.out.print("You can do pretty much anything.");
         }
     }
